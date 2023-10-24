d12a4fe63d8b8b1b067e3fff466c8c411de8382d
JavoKhir124 <95168876+JavoKhir124@users.noreply.github.com>
2023-10-24 16:15:01 +0500

    Portfolio website

---
+++ js/main.js
@@ -0,0 +1,30 @@
+let navbar = document.querySelector('.heading .navbar');
+let menuBtn = document.querySelector('#menu-btn');
+
+menuBtn.onclick = () =>{
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+};
+
+
+
+
+
+const slides = document.querySelector(".slider").children;
+const indicatorImages = document.querySelector(".slider-indicator").children;
+
+for(let i=0; i<indicatorImages.length; i++){
+  indicatorImages[i].addEventListener("click", function(){
+
+    for(let j=0; j<indicatorImages.length; j++){
+      indicatorImages[j].classList.remove("active");
+    }
+    this.classList.add("active");
+    const id=this.getAttribute("data-id");
+    for(let j=0; j<slides.length; j++){
+      slides[j].classList.remove("active");
+    }
+
+    slides[id].classList.add("active");
+  })
+}
